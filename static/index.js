a471456e16c54b79f981dbc7494dd1d3462ad7e4
Eduardo <hurtado.josegomez@gmail.com>
2024-04-22 21:39:17 -0600

    code generation

--- static/index.js
+++ static/index.js
@@ -1,12 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('#signInButton').addEventListener('click', () => {
+        signInFunction();
+    });
+
+})
+
+// ------- Own code ------
+
+function signInFunction() {
+    alert('Pressed');
+}
+
+// --------------------
+
+// Code Verifier 
 const generateRandomString = (length) => {
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const values = crypto.getRandomValues(new Uint8Array(length));
   return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 }
 
+// Code challenge
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+// Base 64 generator
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+
+// Examples
 const codeVerifier  = generateRandomString(64);
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
 
 
-console.log(codeVerifier)
 
 
