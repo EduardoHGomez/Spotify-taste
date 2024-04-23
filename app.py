c939f5ade7552b5d16cba67f5e95cdc76a487c35
Eduardo <hurtado.josegomez@gmail.com>
2024-04-22 20:34:03 -0600

    Sign In button

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/redirect")
+def redirect():
+    return "Redirect link"
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
