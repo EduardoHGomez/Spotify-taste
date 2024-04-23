13a222dc2082adf28aa67edcad86ebcec52c17fd
Eduardo <hurtado.josegomez@gmail.com>
2024-04-22 23:59:48 -0600

    redirect link working

--- static/index.js
+++ static/index.js
@@ -1,7 +1,70 @@
+const clientId = '8d6a62d1e85e412d9f237a08f9ee13ee';
+const redirectUri = 'http://localhost:3000/';
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+
+// ------------ Redirect functions -------------------
+
+console.log(code);
+console.log(localStorage.access_token);
+// After the user is redirected once accepted the petition, it is given back to code
+const getToken = async code => {
+
+    const url = `https://accounts.spotify.com/api/token`;
+
+    // stored in the previous step let codeVerifier = localStorage.getItem('code_verifier');
+
+    const payload = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            client_id: clientId,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier,
+        }),
+
+
+    }
+
+  const body = await fetch(url, payload);
+  const response = await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+
+}
+
+
+if (code) {
+    localStorage.setItem("code_redirect", code);
+
+    // Check if token exists
+    if (localStorage.access_token) {
+        window.location.href = 'http://localhost:3000';
+    } else {
+        getToken(code);
+    }
+}
+
+
+// ---------- End of redirect functions -------------------------
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('#signInButton').addEventListener('click', () => {
         signInFunction();
+    });
+
+    document.querySelector('#getProfileButton').addEventListener('click', () => {
+        getProfile();
     });
 
 })
@@ -14,8 +77,6 @@ async function signInFunction() {
     const hashed = await sha256(codeVerifier)
     const codeChallenge = base64encode(hashed);
 
-    const clientId = '8d6a62d1e85e412d9f237a08f9ee13ee';
-    const redirectUri = 'http://localhost:3000/redirect';
 
     const scope = 'user-read-private user-read-email';
     const authUrl = new URL("https://accounts.spotify.com/authorize")
@@ -63,3 +124,19 @@ const base64encode = (input) => {
 
 
 
+// Example of usage
+async function getProfile(accessToken) {
+
+    console.log(localStorage.getItem('access_token'));
+
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + localStorage.getItem('access_token')
+    }
+  });
+
+  console.log(response);
+
+  const data = await response.json();
+
+}
