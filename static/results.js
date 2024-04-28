80c1b6ff461fee372daf60e24f5cd9337e91d6a4
Eduardo <hurtado.josegomez@gmail.com>
2024-04-27 23:55:07 -0600

    slider before

--- static/results.js
+++ static/results.js
@@ -1,8 +1,17 @@
-
-main();
-
-function main() {
+document.addEventListener('DOMContentLoaded', () => {
+    
     let data = JSON.parse(localStorage.getItem('results'));
-    console.log(data);
+    let slide_track = document.querySelector('.slide-track');
+    
+    for(let i=0; i<15; i++) {
+        console.log(data.user_result.image[i]);
+        let new_image = `
+        <div class="slide">
+            <img src="${data.user_result.image[i]}">
+        </div>`
 
-}
+        slide_track.innerHTML += new_image;
+    }
+
+});
+
