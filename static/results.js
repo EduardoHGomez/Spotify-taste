3aa42b740d7778235aa1ea7b4fb19b336d53fad0
Eduardo <hurtado.josegomez@gmail.com>
2024-04-28 03:02:24 -0600

    contaire left to change

--- static/results.js
+++ static/results.js
@@ -12,21 +12,62 @@ document.addEventListener('DOMContentLoaded', () => {
         slide_track.innerHTML += new_image;
     }
 
+    typeWriter('Resultados de tu análisis musical 🧐', '.header_text', 50, function() {
+        setTimeout(function() {
+            loadResults(data.result);
+        }, 1200);
+    });
 
 
 
 });
 
-var i = 0;
-var txt = 'Resultados de tu análisis musical 🧐';
-var speed = 50;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.querySelector(".header_text").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+function typeWriter(text, selector, speed, callback) {
+    let i = 0; // Initialize the counter to 0
+    function type() {
+        if (i < text.length) {
+            document.querySelector(selector).innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        } else if (callback) {
+            callback(); // Call the callback function if provided
+        }
+    }
+    setTimeout(type, speed); // Start the typing effect
 }
 
-setTimeout(typeWriter, 50);
+
+
+function loadResults(data) {
+    let container = document.querySelector('.middle_text');
+    let new_text = document.createElement('div');
+
+    // Danceability
+    let danceability = data.danceability['0'];
+    danceability = Number(danceability * 100).toFixed(2);         // 1.00
+    new_text.innerHTML = `<p><b>El porcentaje de que bailes con tu música 💃 </b> <br> ${danceability}</p>`;
+    container.append(new_text);
+
+    // Energy
+    new_text = document.createElement('div');
+    let energy = data.energy['0'];
+    energy = Number(energy * 100).toFixed(2);         // 1.00
+    new_text.innerHTML = `<p> <b>El porcentaje de energía 🏋 </b> <br> ${energy} </p>`;
+    container.append(new_text);
+
+
+    // Loudness
+    new_text = document.createElement('div');
+    let loudness = data.loudness['0'];
+    loudness = Number(loudness * 100).toFixed(2);         // 1.00
+    new_text.innerHTML = `<p> <b>Porcentaje de pesada que es tu música 📢 </b> <br> ${loudness} </p>`;
+    container.append(new_text);
+
+    // Valence
+    new_text = document.createElement('div');
+    let valence = data.valence['0'];
+    valence = Number(valence).toFixed(2);         // 1.00
+    new_text.innerHTML = `<p> <b>Porcentaje de felicidad que tienes 😊 </b> <br> ${valence} </p>`;
+    container.append(new_text);
+
+}
