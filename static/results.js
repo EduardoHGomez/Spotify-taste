9e7844bd49fc8a2561c1ab4bb69c997edae89813
Eduardo <hurtado.josegomez@gmail.com>
2024-04-28 01:02:27 -0600

    typing effect

--- static/results.js
+++ static/results.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     let slide_track = document.querySelector('.slide-track');
     
     for(let i=0; i<15; i++) {
-        console.log(data.user_result.image[i]);
         let new_image = `
         <div class="slide">
             <img src="${data.user_result.image[i]}">
@@ -13,5 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
         slide_track.innerHTML += new_image;
     }
 
+
+
+
 });
 
+var i = 0;
+var txt = 'Resultados de tu análisis musical 🧐';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".header_text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+setTimeout(typeWriter, 50);
